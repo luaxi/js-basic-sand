563f0da89b50fe7e0f5ad2d60fc05da01fac65b7
Lua <22667586+luaxi@users.noreply.github.com>
2024-05-04 13:26:33 -0300

    Implemented colorful sand and prevent sand over sand.
    Colorful sand: each new particle has a different color.
    Sand over sand: now it's impossible to draw sand when there's already sand.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,6 +3,7 @@ import * as Constants from './constants/constants.js';
 
 let canvasId = "sand-canvas"
 let myMatrix = createEmptyMatrix();
+let colorHue = 1;
 
 console.log(myMatrix);
 
@@ -20,7 +21,7 @@ function main(){
         let x = Math.floor(e.offsetX / Constants.CANVAS_PIXEL_SIZE);
         let y = Math.floor(e.offsetY / Constants.CANVAS_PIXEL_SIZE);
 
-        myMatrix[x][y] = 1;
+        if(myMatrix[x][y] == 0) myMatrix[x][y] = colorHue;
     }
 
     run(myCanvas);
@@ -30,17 +31,19 @@ function run(canvas){
 
     let newMatrix = createEmptyMatrix();
 
+    colorHue = (colorHue > 360) ? 1 : colorHue+1;
+
     for (let i = 0; i < myMatrix.length; i++) {
         for (let j = 0; j < myMatrix[0].length; j++) {
-            if(myMatrix[i][j] == 1){
+            if(myMatrix[i][j] > 0){
                 if (myMatrix[i][j+1] == 0) { // fall down
-                    newMatrix[i][j+1] = 1
+                    newMatrix[i][j+1] = myMatrix[i][j];
                 } else if (i > 0 && myMatrix[i-1][j+1] == 0){ // fall left
-                    newMatrix[i-1][j+1] = 1;
+                    newMatrix[i-1][j+1] = myMatrix[i][j];
                 } else if (i < myMatrix.length-1 && myMatrix[i+1][j+1] == 0) { // fall right
-                    newMatrix[i+1][j+1] = 1;
+                    newMatrix[i+1][j+1] = myMatrix[i][j];
                 } else { // não se move
-                    newMatrix[i][j] = 1;
+                    newMatrix[i][j] = myMatrix[i][j];
                 }
             }
         }
@@ -61,7 +64,7 @@ function renderCanvas(canvas) {
             if (myMatrix[i][j] == 0) {
                 canvas.drawPixel(i, j, Constants.CANVAS_COLOR_EMPTY);
             } else {
-                canvas.drawPixel(i, j, "white");
+                canvas.drawPixel(i, j, `hsl(${myMatrix[i][j]}, 100%, 50%)`);
             }
         }
     }
